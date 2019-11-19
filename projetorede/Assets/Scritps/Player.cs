a67bc53fb4ae57dd8622cf2f1913af72ff8fc259
patriciamesttre <patriciamesttre@hotmail.com>
2019-11-19 20:04:04 -0300

    Inicio da criação projeto rede

---
+++ projetorede/Assets/Scritps/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical")> 0)
+        {
+            transform.Translate(Vector3.forward * 0.5f);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.back * 0.5f);
+        }
+
+        if (Input.GetAxis("horizontal") > 0)
+        {
+            transform.Rotate(Vector3.up * 0.5f);
+        }
+
+        if (Input.GetAxis("horizontal") > 0)
+        {
+            transform.Rotate(Vector3.up * -0.5f);
+        }
+
+
+    }
+}
